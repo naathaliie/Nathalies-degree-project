9e7782eeab54a2f9444424d7fde02262137840c9
naathaliie <nathalielarsson@live.se>
2025-01-26 09:30:57 +0100

    setup

---
+++ pet-care/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import ProductCard from "./components/productCard";
+
+export default function Home() {
+  return (
+    <main>
+        <h1>Hello world!</h1>
+        {/* Använd Link istället för en a-tagg */}
+        <Link href="/users">Users</Link>
+        <ProductCard />
+    </main>
+  )
+}
