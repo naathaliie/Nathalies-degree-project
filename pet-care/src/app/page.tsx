65f87737ee3b2425da76f1b6b3bd2dc6ce4aa451
naathaliie <nathalielarsson@live.se>
2025-02-02 11:54:20 +0100

    fixing

--- pet-care/src/app/page.tsx
+++ pet-care/src/app/page.tsx
@@ -4,10 +4,10 @@ import ProductCard from "./components/productCard";
 export default function Home() {
   return (
     <main>
-        <h1>Hello world!</h1>
-        {/* Använd Link istället för en a-tagg */}
-        <Link href="/users">Users</Link>
-        <ProductCard />
+      <h1>Hello world!</h1>
+      {/* Använd Link istället för en a-tagg */}
+      <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
-  )
+  );
 }
