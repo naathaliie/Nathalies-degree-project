158db36d92e741192f6716cd7b6f9d136d15bdce
naathaliie <nathalielarsson@live.se>
2025-02-06 09:14:24 +0100

    basic layout fix

--- pet-care/src/app/layout.tsx
+++ pet-care/src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+/* import { Geist, Geist_Mono } from "next/font/google";
+ */import "./globals.css";
 import TopBar from "./components/TopBar";
 import NavBar from "./components/NavBar";
 
-const geistSans = Geist({
+/* const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
@@ -12,12 +12,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-});
+}); */
 
 export const metadata: Metadata = {
-  title: "PetCare - Din partner för husdjursvård",
+  title: "PetCare",
   description:
-    "PetCare hjälper dig att hålla ditt husdjur friskt och lyckligt. Planera veterinärbesök, få expertråd och hantera ditt husdjurs dagliga behov enkelt.",
+    "PetCare hjälper dig...",
 };
 
 export default function RootLayout({
@@ -28,15 +28,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-dvh flex flex-col mx-auto w-full
-         3xl:w-3/4`}
+        className=" antialiased min-h-screen flex flex-col mx-auto w-full
+         3xl:w-3/4"
       >
         <header className="flex flex-col justify-center items-center h-36">
           <TopBar />
           <NavBar />
         </header>
 
-        <main className="flex-grow">{children}</main>
+        <main className="flex-grow px-5">{children}</main>
 
         <footer className=" bg-petCare-darkBlue text-white p-4 text-center">
           <p>
