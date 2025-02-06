9788885937b70b0719a62cbf57af533bc61850f9
naathaliie <nathalielarsson@live.se>
2025-02-06 09:14:53 +0100

    changed name of component

--- pet-care/src/app/page.tsx
+++ pet-care/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import ProductCard from "./components/productCard";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <main>
       <h1>Hello world!</h1>
