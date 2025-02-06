bc52f3abed6f8bdf6fa9dc448f8d512f687abd19
naathaliie <nathalielarsson@live.se>
2025-02-06 09:14:39 +0100

    height fix

--- pet-care/src/app/not-found.tsx
+++ pet-care/src/app/not-found.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 
 const NotFoundPage = () => {
   return (
-    <div className="mainStyle">
+    <div className="mainStyle !min-h-[80vh]">
       <h1>Hoppsan sidan hittades inte</h1>
       <GoBackBtn />
     </div>
