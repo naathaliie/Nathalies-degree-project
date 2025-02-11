c4d2da95f3499571ad967b706a93dfa58446dd0a
naathaliie <nathalielarsson@live.se>
2025-02-11 10:15:19 +0100

    small setup for page

--- pet-care/src/app/login/page.tsx
+++ pet-care/src/app/login/page.tsx
@@ -1,23 +1,42 @@
 'use client';
-import React from "react";
+import React, { useRef } from "react";
 import "../globals.css";
 import LoginForm from "../components/LoginForm";
-import { useSelector, UseSelector } from "react-redux";
-import { RootState } from "@/lib/store";
+import { useSelector } from "react-redux";
 import { User } from "../../../types/types";
+import { Button } from "@/components/ui/button";
+import { RootState } from "@/lib/store";
+import { useAppDispatch } from "@/lib/hooks";
+import { setCurrentUser } from "@/lib/features/auth/authSlice";
+import { useRouter } from "next/navigation";
 
 const LogInPage = () => {
-
+    const router = useRouter()
     const users = useSelector((state: RootState) => state.users.users)
+    const currentUser = useSelector((state: RootState)=>state.auth.currentUser)
+    const choosenUser = useRef<User | null>(null)
+
+    const dispatch = useAppDispatch();
+
+    const setLoggedInUser = (user:User) => {
+       dispatch(setCurrentUser(user))
+       router.push("/users")
+    }
 
   return (
     <main className="LogInPage flex flex-col justify-center items-center m-5 min-h-[70vh]">
       <h2>Logga in</h2>
-      <ul>
+      <h3>Användare från redux</h3>
+      <h3>Nuvarande inloggad är : {currentUser.username}</h3>
+
+      <ul className="flex flex-col gap-2 m-7">
       {users.map((user: User)=> (
-        <li key={user.id}>{user.username}</li>
+        <li key={user.id}>
+            <Button onClick={()=> {choosenUser.current = user; console.log("Du klickade på: ", choosenUser)}}>{user.username}</Button>
+        </li>
       ))}
       </ul>
+     <Button onClick={()=> choosenUser.current ? setLoggedInUser(choosenUser.current) : alert("Du måste välja en användare")} className="mb-5">Tillfällig logga in</Button> {/* TA BORT SEDAN */}
       <LoginForm/>
     </main>
   );
--- pet-care/src/app/users/page.tsx
+++ pet-care/src/app/users/page.tsx
@@ -1,8 +1,31 @@
+'use client';
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/lib/store'
 
 const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
+
+const currentUser = useSelector((state: RootState)=>state.auth.currentUser)
+
+
+  return (<>
+    <div className='LogInPage flex flex-col items-center min-h-[70vh]'>
+        <div className='bg-petCare-mainBlue bg-opacity-25 w-full h-36 flex flex-col justify-center mb-5'>
+            <div className='ml-10'>
+                <h1 className=' text-xl font-semibold sm:text-3xl md:text-5xl'>Välkommen {currentUser.username}!</h1>
+                <h3 className='text-sm md:text-xl'>Här kan du se dina husdjur, lägga till ett nytt husdjur och följa dina blablabla...</h3>
+            </div>
+        </div>
+         <div>
+            <p>Dina registrerade husdjur</p>
+            <ul>
+                {currentUser.pets.map((pet)=> {
+                    return <li key={pet.id}>{pet.name}</li>
+                })}
+            </ul>
+        </div>
+    </div>
+    </>
   )
 }
 
