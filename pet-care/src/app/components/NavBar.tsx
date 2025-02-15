eaff25d12d0f7f8f3a6104d8ba6e8169eaff3138
naathaliie <nathalielarsson@live.se>
2025-02-15 09:51:49 +0100

    code cleanup

--- pet-care/src/app/components/NavBar.tsx
+++ pet-care/src/app/components/NavBar.tsx
@@ -1,11 +1,8 @@
-"use client"; // TA BORT SEN, BARA FÖR ATT VISA CURRENTUSER
 import React from "react";
 import { RootState } from "@/lib/store";
 import { useSelector } from "react-redux";
 
 const NavBar = () => {
-  const currentUser = useSelector((state: RootState) => state.auth.currentUser);
-
   const navItems: String[] = [
     "Alternativ 1",
     "Alternativ 2",
@@ -15,9 +12,6 @@ const NavBar = () => {
   ];
   return (
     <nav className="NavBar w-full h-1/3 flex justify-center bg-petCare-darkBlue text-white">
-      <div className="self-center mr-10">
-        <p className="text-xs">Current user: {currentUser.username}</p>
-      </div>
       <div className=" flex items-center">
         <ul className="flex gap-5">
           {navItems.map((item, index) => {
