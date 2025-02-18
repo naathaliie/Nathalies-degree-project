08c2ebec42729fb92f636212421506932be888f0
naathaliie <nathalielarsson@live.se>
2025-02-18 15:30:17 +0100

    removed connectToDb, no longer needed here

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,9 +1,7 @@
 import Express from "express"
 import Mongoose from "mongoose"
 
-export async function ConnectToDB() {
-    await Mongoose.connect("mongodb://localhost:27017/")
-}
+
 
 export function userRouter(){
     const router = Express.Router()
