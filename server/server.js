400e5b446f173bccb3c30f0aa1c78cbf5f259499
naathaliie <nathalielarsson@live.se>
2025-02-18 15:29:59 +0100

    importet userRouter and updated to new funktion to connect to DB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import Express from "express"
-import { ConnectToDB } from "./routes/userRouter";
+import { userRouter } from "./routes/userRouter.js";
+import { connectToDB } from "./dataBase/connectToDB.js";
 
 const app = Express()
 const port = 3003
@@ -8,13 +9,12 @@ app.use(Express.json())//Middleware för att parsa inkommande JSON-data
 
 //Koppla upp mot databasen för ALLA resurser
 app.use("*", async (req, res, next) => {
-    await ConnectToDB();
+    await connectToDB();
     next();
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello world, det fungerar yeey!") //Skickar ett enkelt svar på GET
-})
+//Få tillgång till alla endpoints
+app.use("/users", userRouter())
 
 app.listen(port, ()=>{
 console.log(`Server is running on port: ${port}`)
