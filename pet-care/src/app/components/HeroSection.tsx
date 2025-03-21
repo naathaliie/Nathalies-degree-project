2082af728f8c1eb96ac5d287bafff3359e5950ce
naathaliie <nathalielarsson@live.se>
2025-03-21 16:23:58 +0100

    continu work on submitForm

--- pet-care/src/app/components/HeroSection.tsx
+++ pet-care/src/app/components/HeroSection.tsx
@@ -1,15 +1,52 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { PartialPetFirst } from '../../../types/types'
 
 const HeroSection = () => {
-const [selectedPet, setSelectedPet] = useState<string>("Hund")
 const pets = ["Hund", "Katt", "Häst", "Smådjur"]
 
-const handleSelectedPet = (pet:string) => {
+const [selectedPet, setSelectedPet] = useState<string>("Hund")
 
+const [petBirthdayInput, setPetBirthdayInput] = useState('')
+const [petNameInput, setPetNameInput] = useState('')
+const [petBreedInput, setPetBreedInput] = useState('')
+const [petGenderInput, setPetGenderInput] = useState('')
+const [userSecurityNumbInput, setUserSecurityNumbInput] = useState('')
+
+const [newPet, setNewPet] = useState<PartialPetFirst>(
+    {
+        name: '',
+        species: '',
+        breed: '',
+        sex: '',
+        birthday: '',
+    }
+)
+
+const handleSelectedPet = (pet:string) => {
     setSelectedPet(pet)
-    console.log("selectedPet är:", selectedPet)
+}
+
+//Validering på inputfälten!
+
+const submitForm = () => {
+    setNewPet({
+        name: petNameInput,
+        species: selectedPet,
+        breed: petBreedInput,
+        sex: petGenderInput,
+        birthday: petBirthdayInput,
+    })
+
+    //Skicka med userns ssn
+
+    //Sedan ska jag väl skicka newPet till storen för pet och userns id till userstore för att komma åt datan i nästa sida där jag kommer få mer info
+    //På den sidan ska jag kolla om en user finns, får den logga in, annars fortsätta fylla i resten av all info
+
+    //Givetvis bli omdirigerad till den andra vyn också som inte är skapad ännu
+
+    //Rensa alla inputfält
 }
 
 
@@ -24,33 +61,53 @@ const handleSelectedPet = (pet:string) => {
             return <Button key={index} variant={'primary'} size={"default"} className=' font-bold shadow-md shadow-gray-400' onClick={() => handleSelectedPet(pet)}>{pet}</Button>
            })}
         </div>
-        <div className='grid grid-cols-2 gap-2 mt-7 max-w-xl'>
+        <form className='InputForm grid grid-cols-2 gap-2 mt-7 max-w-2xl'>
             <div>
                 <p className=' text-lg font-bold'>{selectedPet === "Smådjur" ? selectedPet + "ets" : selectedPet + "ens"} födelsedatum</p>
-                <input className=' border-2 border-petCare-sapphireTeal-dark' type="date" />
+                <input className=' border-2 border-petCare-sapphireTeal-dark' type="date"  onChange={(e) => setPetBirthdayInput(e.target.value)}/>
             </div>
             <div>
             <p className=' text-lg font-bold'>{selectedPet === "Smådjur" ? selectedPet + "ets" : selectedPet + "ens"} namn</p>
-                <input className=' border-2 border-petCare-sapphireTeal-dark' type="text" />
+                <input className=' border-2 border-petCare-sapphireTeal-dark' type="text" onChange={(e) => setPetNameInput(e.target.value)} />
                 </div>
             <div>
-                <p className=' text-lg font-bold'>Personnummer</p>
-                <input className=' border-2 border-petCare-sapphireTeal-dark' type="date" />
+                <p className=' text-lg font-bold'>Personnummer registrerad ägare</p>
+                <input className=' border-2 border-petCare-sapphireTeal-dark' type="date" onChange={(e) => {setUserSecurityNumbInput(e.target.value)}}/>
             </div>
             <div>
                 <p className=' text-lg font-bold'>{selectedPet === "Smådjur" ? selectedPet + "ets" : selectedPet + "ens"} ras</p>
-                <input className=' border-2 border-petCare-sapphireTeal-dark' type="text" />
+                <input className=' border-2 border-petCare-sapphireTeal-dark' type="text" onChange={(e) => setPetBreedInput(e.target.value)}/>
             </div>
             <div>
                 <p className=' text-lg font-bold'>Kön</p>
-                <select className=' border-2 border-petCare-sapphireTeal-dark'>
-                    <option value="" disabled>Välj kön</option>
+                <select
+                onChange={(e) => setPetGenderInput(e.target.value)}
+                className=' border-2 border-petCare-sapphireTeal-dark'
+                >
+                    <option value="">--Välj kön--</option>
                     <option value="Hona">Hona</option>
                     <option value="Hane">Hane</option>
                 </select>
             </div>
+        </form>
+        <div className='mt-5'>
+            <Button
+            onClick={() => submitForm()}
+            variant={"primary"}
+            size={"lg"}
+            className='font-bold text-xl shadow-md shadow-gray-400'>Registrera</Button>
         </div>
-        <div className='mt-5 fo'><Button variant={"primary"} size={"lg"} className='font-bold text-xl shadow-md shadow-gray-400'>Registrera</Button></div>
+        <div>
+  <p>Du vill lägga till:</p>
+  <ul>
+    <li>Namn: {newPet.name}</li>
+    <li>Art: {newPet.species}</li>
+    <li>Ras: {newPet.breed}</li>
+    <li>Kön: {newPet.sex}</li>
+    <li>Födelsedag: {newPet.birthday.toLocaleString()}</li>
+    <li>User: {userSecurityNumbInput}</li>
+  </ul>
+</div>
     </div>
   )
 }
