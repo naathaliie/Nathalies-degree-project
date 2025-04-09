95f6e8934ccedbe11b3d3c47829c4e3d5ffdeb05
naathaliie <nathalielarsson@live.se>
2025-04-09 16:57:30 +0200

    newUserPage

--- pet-care/src/app/users/new/page.tsx
+++ pet-care/src/app/users/new/page.tsx
@@ -1,4 +1,7 @@
 'use client'
+import RegisterPetForm from '@/app/components/RegisterForms/RegisterPetForm'
+import RegisterUserForm from '@/app/components/RegisterForms/RegisterUserForm'
+import { Button } from '@/components/ui/button'
 import { RootState } from '@/lib/store'
 import React from 'react'
 import { useSelector } from 'react-redux'
@@ -7,7 +10,36 @@ const NewUserPage = () => {
 
     const currentSelectedPet = useSelector((state: RootState) => state.pets.selectedPet)
   return (
-    <div>Välkommen att registrera dig nya användare, valt djur var {currentSelectedPet}</div>
+    <div className='NewUserPage bg-gradient-to-r from-[#C5E3E9] to-[#F9FCFD] bg-opacity-25 p-5 min-h-[70vh] text-petCare-sapphireTeal-dark'>
+        <div className='flex flex-col items-center'>
+            <h1 className='text-3xl font-bold'>Välkommen till oss på PetCare</h1>
+            <h2 className=''>För att kunna ta hand om dig och din {currentSelectedPet} på bästa sätt behöver du fylla i några uppgifter</h2>
+        </div>
+
+        <div className='FormBox m-10 p-10 bg-petCare-myWhite'>
+            <div className='flex flex-col gap-10 md:flex-row justify-evenly'>
+                <div className='md:w-1/2'>
+                    <h1 className=' text-2xl font-bold mb-5'>Uppgifter om dig</h1>
+                    <RegisterUserForm/>
+                </div>
+                <div className='md:w-1/2'>
+                    <h1 className=' text-2xl font-bold mb-5'>Uppgifter om ditt husdjur</h1>
+                    <RegisterPetForm/>
+                </div>
+            </div>
+            <div className='mt-7 flex justify-center'>
+                <Button
+                    variant={"primary"}
+                    size={"lg"}
+                    className="font-bold text-xl shadow-md shadow-gray-400"
+                    onClick={() => console.log("hej")}
+                >
+                    Registrera
+                </Button>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
