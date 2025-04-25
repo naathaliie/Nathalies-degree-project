34c03293acfca5371310b679da2605303e230c7e
naathaliie <nathalielarsson@live.se>
2025-04-25 13:14:45 +0200

    cleanup

--- pet-care/src/app/StoreProvider.tsx
+++ pet-care/src/app/StoreProvider.tsx
@@ -1,10 +1,14 @@
-'use client'
-import { useRef } from 'react'
-import { Provider } from 'react-redux'
-import { makeStore, AppStore } from '../lib/store'
+"use client";
+import { useRef } from "react";
+import { Provider } from "react-redux";
+import { makeStore, AppStore } from "../lib/store";
 
-export default function StoreProvider({ children }: { children: React.ReactNode }) {
-  const storeRef = useRef<AppStore>(makeStore()) //Direkt initiering istället för if-sats
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const storeRef = useRef<AppStore>(makeStore());
 
-  return <Provider store={storeRef.current}>{children}</Provider>
+  return <Provider store={storeRef.current}>{children}</Provider>;
 }
