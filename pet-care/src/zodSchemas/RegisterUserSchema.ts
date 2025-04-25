b5656a4d98ba99da5076956e94b7dc4375ace116
naathaliie <nathalielarsson@live.se>
2025-04-25 11:43:38 +0200

    small changes

--- pet-care/src/zodSchemas/RegisterUserSchema.ts
+++ pet-care/src/zodSchemas/RegisterUserSchema.ts
@@ -1,8 +1,10 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const RegisterUserSchema = z.object({
   email: z.string().email({ message: "Måste vara en giltig e-postadress" }),
-  password: z.string().min(2, { message: "Lösenord måste vara minst 2 tecken" }),
+  password: z
+    .string()
+    .min(2, { message: "Lösenord måste vara minst 2 tecken" }),
   name: z.string().min(1, { message: "Namn får inte vara tomt" }),
   surname: z.string().min(1, { message: "Efternamn får inte vara tomt" }),
   street: z.string().min(1, { message: "Adress får inte vara tomt" }),
