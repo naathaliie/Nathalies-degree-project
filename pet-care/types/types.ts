f91b41bce43a140afc930f22624d3c6a737b821a
naathaliie <nathalielarsson@live.se>
2025-04-25 10:04:24 +0200

    cleanup in types

--- pet-care/types/types.ts
+++ pet-care/types/types.ts
@@ -1,70 +1,60 @@
 /*****User*****/
 export interface User {
-    _id?: string //Skapas först när man skickat in till DB och finns inte innan
-    email: string
-    password: string
-    dateOfRegistration: Date | string
-    name: string
-    surname: string
-    street: string
-    city: string
-    postalCode: string
-    phone?: string | undefined
-    pets?: any[] | Pet[]
-    orders?: any[] | undefined
-    favorites?: any[] | undefined
-    messages?: any[] | undefined
-  }
+  _id: string;
+  email: string;
+  password: string;
+  name: string;
+  surname: string;
+  street: string;
+  city: string;
+  postalCode: string;
+  phone?: string | undefined;
+  pets?: string[]; //Husdjurets id
+}
 
 export interface UsersState {
-    users: User[]
-    loading: {
-        getUsers: boolean,
-        postNewUser: boolean,
-    }
-    error: string | null
-  }
-
+  users: User[];
+  loading: {
+    getUsers: boolean;
+    postNewUser: boolean;
+  };
+  error: string | null;
+}
 
 /*****CurrentUser*****/
 export interface AuthState {
-    currentUser: User & {isLoggedIn: boolean};
-  }
+  currentUser: (User & { isLoggedIn: boolean }) | null;
+}
 
 /*****Pet*****/
 export interface Pet {
-    _id?: string //Skapas först när man skickat in till DB och finns inte innan
-    ownerId: string
-    dateOfRegistration: Date | string
-    name: string
-    species: ChoosablePets | null
-    breed: string
-    sex: string
-    birthday?: Date | string
-    description?: string[],
-    healthInfo?: HealthInfo[]
+  _id?: string;
+  ownerId: string;
+  name: string;
+  species: ChoosablePets | null;
+  breed: string;
+  gender: string;
+  birthday: Date | string;
 }
 
 export interface PetsState {
-    pets: Pet[]
-    selectedPet: ChoosablePets | null
-    loading: boolean
-    error: string | null
+  pets: Pet[];
+  selectedPet: ChoosablePets | null;
+  loading: boolean;
+  error: string | null;
 }
-
 
 export const choosablePetsArray = ["Hund", "Katt", "Häst", "Smådjur"] as const; // Här lägger vi till fler djur vid behov
 
-export type ChoosablePets = typeof choosablePetsArray[number];
-
+export type ChoosablePets = (typeof choosablePetsArray)[number];
 
 /*****HealthInfo*****/
 
 export interface HealthInfo {
-    petId: string
-    dateOfRegistration: Date
-    type: "Veterinär" | "Pälsvård" | "Kurs"
-    date: Date
-    notes: string
-    place: string,
+  petId: string;
+  dateOfRegistration: Date;
+  type: "Veterinär" | "Pälsvård" | "Kurs";
+  date: Date;
+  notes: string;
+  place: string;
 }
