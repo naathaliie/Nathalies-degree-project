9f594389b99b558b91c447d7b0a94f1bcfc809a0
naathaliie <nathalielarsson@live.se>
2025-04-25 10:16:14 +0200

    made custom button

---
+++ pet-care/src/app/components/Buttons/PetCareButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Button, ButtonProps } from "@mui/material";
+
+interface PetCareButtonProps extends ButtonProps {
+  icon?: React.ReactNode; // Valfri ikon
+  label: string; // Texten på knappen
+}
+
+//En vanlig knapp
+//Du kan skicka in loading/disabled/type/onClick/size osv som vanligt,
+//jag har satt vissa attribut som default här icon och label är mandatory
+
+const PetCareButton: React.FC<PetCareButtonProps> = ({
+  icon,
+  label,
+  className,
+  ...props
+}) => {
+  return (
+    <Button
+      endIcon={icon}
+      variant="contained"
+      size="large"
+      loadingPosition="end"
+      className={`${className} !bg-petCare-sapphireTeal-dark w-fit !font-bold  !text-white disabled:!bg-gray-400 disabled:!text-gray-200 `}
+      {...props}
+    >
+      {label}
+    </Button>
+  );
+};
+
+export default PetCareButton;
