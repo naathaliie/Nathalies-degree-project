2b55969a8038b9091f5b475bd8fded1b11793420
naathaliie <nathalielarsson@live.se>
2025-05-09 14:53:52 +0200

    added links

--- pet-care/src/app/components/MyPageNav.tsx
+++ pet-care/src/app/components/MyPageNav.tsx
@@ -2,32 +2,69 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/lib/store";
+import Link from "next/link";
+import { useAppDispatch } from "@/lib/hooks";
+import { logout } from "@/lib/features/auth/authSlice";
+import { ArrowRightStartOnRectangleIcon } from "@heroicons/react/24/solid";
 
 const MyPageNav = () => {
-  const currentUser = useSelector((state: RootState) => state.auth.currentUser);
+  const dispatch = useAppDispatch();
 
-  const navItems: String[] = [
-    "Mina sidor",
-    "Profil",
-    "Mina djur",
-    "Alternativ 4",
-    "Alternativ 5",
+  const currentUser = useSelector((state: RootState) => state.auth.currentUser);
+  const navItems = [
+    {
+      titel: "Mina sidor",
+      href: "/users/",
+    },
+    {
+      titel: "Mina husdjur",
+      href: "/users/myPets",
+    },
+    {
+      titel: "Redigera profil",
+      href: "/users/editProfile",
+    },
+    {
+      titel: "Logga ut",
+      href: "",
+    },
   ];
+
+  const handleLogOut = () => {
+    dispatch(logout());
+  };
 
   return (
     <>
       {currentUser?.isLoggedIn && (
         <nav className="NavBar w-full p-2 flex justify-center bg-petCare-sapphireTeal-light text-white">
           <div className=" flex items-center">
-            <div className="self-center">
-              <p className="text-xs">inloggad:{currentUser?.name}</p>
-            </div>
-
-            <ul className="flex gap-5">
+            <div className="flex gap-5">
               {navItems.map((item, index) => {
-                return <li key={index}>{item}</li>;
+                if (item.titel === "Logga ut") {
+                  return (
+                    <Link
+                      href={"/"}
+                      key={index}
+                      onClick={() => handleLogOut()}
+                      className="flex items-center text-petCare-sapphireTeal-dark hover:text-petCare-myWhite"
+                    >
+                      <p>Logga ut</p>
+                      <ArrowRightStartOnRectangleIcon className="size-6 " />
+                    </Link>
+                  );
+                }
+                return (
+                  <Link
+                    href={item.href}
+                    key={index}
+                    className=" hover:text-petCare-sapphireTeal-dark"
+                  >
+                    {item.titel}
+                  </Link>
+                );
               })}
-            </ul>
+            </div>
           </div>
         </nav>
       )}
