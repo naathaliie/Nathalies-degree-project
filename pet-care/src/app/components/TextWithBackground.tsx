e5c348172ec344a240e6e5422ba94ac592a30544
naathaliie <nathalielarsson@live.se>
2025-05-14 15:55:54 +0200

    textWithBackground created

---
+++ pet-care/src/app/components/TextWithBackground.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const TextWithBackground = () => {
+  return (
+    //Lägg till färgen i tailwind så att den inite är statisk
+    <div className="bg-[#C5E3E9]  w-full py-20 pl-16 pr-16 text-petCare-sapphireTeal-dark flex flex-col justify-center items-center ">
+      <div className="w-2/3  flex flex-col gap-5 justify-center items-center ">
+        <h1 className="font-bold text-2xl">
+          Samlingsplatsen för alla husdjursägare!
+        </h1>
+        <p>
+          Vi som jobbar på PetCare är själva djurägare och delar din passion för
+          husdjur. Tillsammans har vi mer än 100 års erfarenhet av djur och vet
+          hur man på bästa sätt tar hand om sin pälskling. Vi vet också hur
+          vanligt det är att glömma bort när man senast gjorde en vaccination
+          eller vilket datum kloklippningen är bokad till. Men du kan vara lugn,
+          vi samlar allt om ditt husdjur på en och samma plats, så att du kan
+          luta dig tillbaka och känna dig trygg med att all viktig dokumentation
+          finns samlat och att vi påminner dig om kloklippningen eller
+          veterinärbesöket.
+        </p>
+        <p>
+          Ring oss gärna på <b>0707 - 12 34 56</b> om du undrar över något.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TextWithBackground;
--- pet-care/src/app/page.tsx
+++ pet-care/src/app/page.tsx
@@ -4,12 +4,14 @@ import dog from "../../public/8630162.png";
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import UspList from "./components/USP/UspList";
+import TextWithBackground from "./components/TextWithBackground";
 
 export default function HomePage() {
   return (
     <main className="HomePage">
       <HeroSection />
       <UspList />
+      <TextWithBackground />
       <h1 className="text-6xl text-petCare-sapphireTeal-dark">
         Välkommen till PetCare
       </h1>
