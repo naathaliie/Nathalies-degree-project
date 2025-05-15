d18054578bd22b415992e80f353c4ce52b07c508
naathaliie <nathalielarsson@live.se>
2025-05-15 10:05:47 +0200

    small fix

--- pet-care/src/app/components/USP/UspCard.tsx
+++ pet-care/src/app/components/USP/UspCard.tsx
@@ -8,12 +8,12 @@ type UspCardProps = {
 const UspCard = ({ title, text }: UspCardProps) => {
   return (
     <div
-      className="relative border-2 border-petCare-sapphireTeal-dark 
-    flex flex-col text-center gap-4 py-5 px-2 w-80 min-h-44 
+      className="relative border-2 border-petCare-sapphireTeal-dark
+    flex flex-col text-center gap-4 py-5 px-2 w-80 min-h-44
     rounded-md   shadow-[8px_8px_15px_rgba(14,76,86,0.50)]"
     >
       <img
-        src="/pawByMe.svg"
+        src="/svg/pawByMe.svg"
         width={40}
         alt="paw"
         className="absolute top-[-25px] left-1/2 transform -translate-x-1/2"
