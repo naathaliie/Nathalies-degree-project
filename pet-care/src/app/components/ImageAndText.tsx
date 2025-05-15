43e383e8e72aa749c4b50ab7a09e4485d35cda4f
naathaliie <nathalielarsson@live.se>
2025-05-15 15:50:48 +0200

    small fix

--- pet-care/src/app/components/ImageAndText.tsx
+++ pet-care/src/app/components/ImageAndText.tsx
@@ -4,9 +4,15 @@ const ImageAndText = () => {
   return (
     <div className=" relative w-full flex flex-col justify-center items-center ">
       <div className="w-full flex flex-col gap-5 justify-center items-center ">
-        <img src="/images/catAndDogGrass.jpg" alt="cat and dog on grass" className=" w-full h-[30rem] object-cover"/>
+        <img
+          src="/images/catAndDogGrass.jpg"
+          alt="cat and dog on grass"
+          className=" w-full h-[30rem] object-cover"
+        />
         <div className=" absolute text-petCare-myWhite">
-            <h1 className=" font-bold text-5xl md:text-7xl">Välkommen till oss!</h1>
+          <h1 className=" font-bold text-5xl md:text-7xl">
+            Välkommen till Petcare!
+          </h1>
         </div>
       </div>
     </div>
