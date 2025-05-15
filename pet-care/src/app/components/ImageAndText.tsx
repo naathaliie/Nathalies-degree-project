4134ab1ab17603af358bd20c7bd12fcfd37aff99
naathaliie <nathalielarsson@live.se>
2025-05-15 10:05:35 +0200

    ImageAndText

---
+++ pet-care/src/app/components/ImageAndText.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ImageAndText = () => {
+  return (
+    <div className=" relative w-full flex flex-col justify-center items-center ">
+      <div className="w-full flex flex-col gap-5 justify-center items-center ">
+        <img src="/images/catAndDogGrass.jpg" alt="cat and dog on grass" className=" w-full h-[30rem] object-cover"/>
+        <div className=" absolute text-petCare-myWhite">
+            <h1 className=" font-bold text-5xl md:text-7xl">Välkommen till oss!</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ImageAndText;
--- pet-care/src/app/page.tsx
+++ pet-care/src/app/page.tsx
@@ -5,10 +5,12 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import UspList from "./components/USP/UspList";
 import TextWithBackground from "./components/TextWithBackground";
+import ImageAndText from "./components/ImageAndText";
 
 export default function HomePage() {
   return (
     <main className="HomePage">
+      <ImageAndText/>
       <HeroSection />
       <UspList />
       <TextWithBackground />
