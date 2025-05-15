f735a25b5cb8d2577f531fa8c6bd3fa3421929e1
naathaliie <nathalielarsson@live.se>
2025-05-15 15:49:49 +0200

    hamburgermenu

---
+++ pet-care/src/app/components/HamburgerMenu.tsx
@@ -0,0 +1,50 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import DehazeIcon from "@mui/icons-material/Dehaze";
+
+type HamburgerMenuProps = {
+  menuItems: string[];
+};
+
+const HamburgerMenu = ({ menuItems }: HamburgerMenuProps) => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <DehazeIcon sx={{ color: "white", width: 30, height: 30 }} />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        {menuItems.map((item, index) => {
+          return (
+            <MenuItem key={index} onClick={handleClose}>
+              {item}
+            </MenuItem>
+          );
+        })}
+      </Menu>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
--- pet-care/src/app/components/NavBar.tsx
+++ pet-care/src/app/components/NavBar.tsx
@@ -1,9 +1,11 @@
+"use client";
 import React from "react";
 import { RootState } from "@/lib/store";
 import { useSelector } from "react-redux";
+import HamburgerMenu from "./HamburgerMenu";
 
 const NavBar = () => {
-  const navItems: String[] = [
+  const navItems: string[] = [
     "Alternativ 1",
     "Alternativ 2",
     "Alternativ 3",
@@ -11,13 +13,18 @@ const NavBar = () => {
     "Alternativ 5",
   ];
   return (
-    <nav className="NavBar w-full h-1/3 flex justify-center bg-petCare-sapphireTeal-dark text-white">
-      <div className=" flex items-center">
-        <ul className="flex gap-5">
-          {navItems.map((item, index) => {
-            return <li key={index}>{item}</li>;
-          })}
-        </ul>
+    <nav className="relative w-full h-1/3 flex justify-center items-center bg-petCare-sapphireTeal-dark text-white">
+      <div className="hidden sm:block">
+        <div className=" flex items-center">
+          <ul className="flex gap-5">
+            {navItems.map((item, index) => {
+              return <li key={index}>{item}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
+      <div className="block sm:hidden absolute right-2 top-[15%] ">
+        <HamburgerMenu menuItems={navItems} />
       </div>
     </nav>
   );
