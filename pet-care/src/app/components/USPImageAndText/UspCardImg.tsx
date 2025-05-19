21ef6a354d613618bcdbc3c364ef4e90605e3d55
naathaliie <nathalielarsson@live.se>
2025-05-19 14:43:40 +0200

    codeclenaup

--- pet-care/src/app/components/USPImageAndText/UspCardImg.tsx
+++ pet-care/src/app/components/USPImageAndText/UspCardImg.tsx
@@ -28,11 +28,3 @@ const UspCardImg = ({ img, imgDescription, title, text }: UspCardImgProps) => {
 };
 
 export default UspCardImg;
-
-/* 6px är för x-förskjutningen (den förskjuter skuggan åt höger).
-
-6px är för y-förskjutningen (den kan justeras beroende på hur mycket du vill ha den nedåt eller uppåt).
-
-20px är suddigheten på skuggan.
-
-rgba(0,0,0,0.2) är skuggans färg med genomskinlighet. */
