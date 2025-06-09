5030cf20011f68544499530c39e22f7c20c7b242
naathaliie <nathalielarsson@live.se>
2025-06-09 14:22:45 +0200

    added textInfo

--- pet-care/src/app/info/ourServices/page.tsx
+++ pet-care/src/app/info/ourServices/page.tsx
@@ -2,7 +2,11 @@
 import React from "react";
 
 const OurServicesPage = () => {
-  return <></>;
+  return (
+    <div className="w-full h-full flex justify-center items-center align-middle text-petCare-sapphireTeal-dark">
+      Välj ett företag för att läsa mer om deras tjänster
+    </div>
+  );
 };
 
 export default OurServicesPage;
--- pet-care/src/app/users/myPets/page.tsx
+++ pet-care/src/app/users/myPets/page.tsx
@@ -2,7 +2,11 @@
 import React from "react";
 
 const MyPetsPage = () => {
-  return <></>;
+  return (
+    <div className="w-full h-full flex justify-center items-center align-middle text-petCare-sapphireTeal-dark">
+      Välj ett husdjur för att se bokade tider och mer
+    </div>
+  );
 };
 
 export default MyPetsPage;
