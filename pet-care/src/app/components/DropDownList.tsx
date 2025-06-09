28e35295cbe8bb06ee8a5cead2ddb5c0a72899c5
naathaliie <nathalielarsson@live.se>
2025-06-09 13:06:19 +0200

    created a dropdown

---
+++ pet-care/src/app/components/DropDownList.tsx
@@ -0,0 +1,41 @@
+import React, { SetStateAction, useState } from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+type DropDownListProps = {
+  listItems: string[];
+  state: string;
+  setState: React.Dispatch<SetStateAction<string>>;
+};
+
+const DropDownList = ({ listItems, state, setState }: DropDownListProps) => {
+  const handleChange = (event: SelectChangeEvent) => {
+    setState(event.target.value as string);
+  };
+  console.log("Vad får jag:", listItems);
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <InputLabel id="dropDownList">Välj tjänst</InputLabel>
+        <Select
+          labelId="dropDownList"
+          id="dropDown"
+          value={state}
+          label="Kategorier"
+          onChange={handleChange}
+        >
+          {listItems.map((item, i) => (
+            <MenuItem key={i} value={item}>
+              {item}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+};
+
+export default DropDownList;
