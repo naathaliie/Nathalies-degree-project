1fb735afce5c28ffd96bd45a51ba5a9ced64bdfb
naathaliie <nathalielarsson@live.se>
2025-06-09 09:23:45 +0200

    added calender to see each pet events

--- pet-care/src/app/users/myPets/[petId]/page.tsx
+++ pet-care/src/app/users/myPets/[petId]/page.tsx
@@ -6,6 +6,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import { useEffect, useState } from "react";
 import RemovePetModal from "@/app/components/RemovePetModal";
 import Tooltip from "@mui/material/Tooltip";
+import Calendar from "@/app/components/Calendar";
 
 const PetDetails = () => {
   const { petId } = useParams() as { petId: string };
@@ -31,7 +32,7 @@ const PetDetails = () => {
     <>
       {foundPet ? (
         <div className="md:m-5">
-          <div className="col-span-1 md:mt-5">
+          <div>
             <div className=" flex text-petCare-sapphireTeal-dark py-2 md:py-5">
               <div className="flex flex-col ml-10 my-5 md:mb-14">
                 <h1 className="text-xl font-semibold sm:text-3xl md:text-3xl">
@@ -42,37 +43,37 @@ const PetDetails = () => {
             </div>
             <div className=" flex flex-col-reverse md:grid md:grid-cols-2 md:grid-flow-col bg-petCare-myWhite rounded-lg px-10 py-5 mx-3 mb-5  md:mx-7 text-petCare-sapphireTeal-dark">
               <div>
-                <div className="flex flex-col gap-7  flex-wrap">
+                <div className="border-b-2 border-petCare-sapphireTeal-superLight pb-7 flex flex-col gap-7 flex-wrap">
                   <div>
                     <h2 className=" font-bold text-lg">Ras:</h2>
                     <p>{foundPet.breed}</p>
                   </div>
-
                   <div>
                     <h2 className=" font-bold text-lg">Kön:</h2>
                     <p>{foundPet.gender}</p>
                   </div>
-                  <div>
-                    <div className="flex flex-col gap-2">
-                      <Tooltip title="Tryck här om du vill ta bort husdjuret (du blir tillfrågad med Ja eller Nej).">
-                        <SaveButton
-                          label="Radera "
-                          icon={<DeleteIcon fontSize="small" />}
-                          state={SaveButtonState}
-                          setState={setSaveButtonState}
-                          size="small"
-                          onClick={() => removePet()}
-                        />
-                      </Tooltip>
-                    </div>
-                  </div>
+                  <Tooltip title="Tryck här om du vill avregistrera husdjuret (du blir tillfrågad med Ja eller Nej).">
+                    <SaveButton
+                      label="Ta bort "
+                      icon={<DeleteIcon fontSize="small" />}
+                      state={SaveButtonState}
+                      setState={setSaveButtonState}
+                      size="small"
+                      onClick={() => removePet()}
+                    />
+                  </Tooltip>
+                </div>
+                <div className="pt-5">
+                  <h2 className=" font-bold text-lg">Inbokade besök</h2>
+                  <Calendar pet={foundPet} />
                 </div>
               </div>
+
               <img
                 src={foundPet.img ? foundPet.img : "/images/noImgFound.png"}
                 width={40}
                 alt={"Bild av husdjur"}
-                className=" h-52 w-72 object-cover rounded-3xl pb-5 md:pb-0"
+                className=" h-52 w-72 object-scale-down rounded-3xl pb-5 md:pb-0"
               ></img>
             </div>
           </div>
